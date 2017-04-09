8c62de504e06a4ca4b5813ca60871fe308e78400
Leonid Petrov <lenia.petrov@gmail.com>
2017-04-09 13:26:04 -0400

    revert back

--- _includes/cal_scripts.js
+++ _includes/cal_scripts.js
@@ -105,9 +105,7 @@ function makeApiCall() {
                   startDayWeek, ' ',
                   startMonth, ' ',
                   startDay, ' ',
-                  startYear, '</a> - ', item.summary, ' in ', item.location,
-                  '</b><br><table><tr><td style="padding-left:40px"><p style="font-size:14px">',
-                  item.description, '</p></td></tr></table>'
+                  startYear, '</a></b> - ', item.summary, ' in <b>', item.location, '</b><br><br>'
                   ];
                 }
                 else{
@@ -120,8 +118,7 @@ function makeApiCall() {
                         startMonth, ' ',
                         startDay, ' ',
                         startYear, ' @ ',
-                        startHour, ':', startMin, '</a> - ', item.summary, ' in ', item.location, '</b><br><table><tr><td style="padding-left:40px"><p style="font-size:14px">',
-                        item.description, '</p></td></tr></table>'
+                        startHour, ':', startMin, '</a></b> - ', item.summary, ' in <b>', item.location, '</b><br><br>'
                         ];
                 }
                 li.innerHTML = str.join('');
